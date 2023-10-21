893eae3f037ec5086cefbe4d5ada75c11848e091
Hamza Khan <hamzakhann66@gmail.com>
2023-10-21 16:56:47 +0500

    added auth mfe

--- container/src/App.js
+++ container/src/App.js
@@ -1,15 +1,36 @@
-import React from "react";
-import MarketingApp from "./components/MarketingApp";
-import { BrowserRouter } from "react-router-dom";
+import React, { Suspense, lazy, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import Header from "./components/Header";
+import Progress from "./components/Progress";
+const MarketingApp = lazy(() => import("./components/MarketingApp"));
+const AuthApp = lazy(() => import("./components/AuthApp"));
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
+  const [isSignedin, setSignedin] = useState(false);
+
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header  onSignOut={()=>setSignedin(false)}  isSignedIn={isSignedin} />
+          <Suspense fallback={<Progress/>}>
+            <Switch>
+              <Route path="/auth">
+                <AuthApp onSignIn={()=>setSignedin(true)}/>
+                </Route>
+              <Route path="/" component={MarketingApp} />
+            </Switch>
+          </Suspense>
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
